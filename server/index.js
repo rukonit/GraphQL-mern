4875d9ce9097551cdd3421b10fc8b859b87caced
rukonit <rukon-it@live.com>
2022-06-23 23:58:53 -0400

    initial upload

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+require('dotenv').config();
+const {graphqlHTTP} = require('express-graphql');
+const schema = require('./schema/schema')
+const port = process.env.PORT || 4000;
+
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+
+}))
+
+app.listen(port, console.log("Listening to port: "+ port));
